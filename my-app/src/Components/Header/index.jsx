130644aacfa14d64f4fb53c15f714deee4f28235
CaptainFlopp3y <barrancofabian@gmail.com>
2023-07-30 23:14:25 -0600

    adding index.jsx/style.jsx time to finish those codes till i commit again hehehehehe

---
+++ my-app/src/Components/Header/index.jsx
@@ -0,0 +1,13 @@
+import { HeaderContainer, HeaderTitle, Subheader } from './style';
+import React from 'react';
+
+const Header = () => {
+    return (
+        <HeaderContainer>
+            <HeaderTitle>React Portfolio</HeaderTitle>
+            <Subheader> Fabian Barranco </Subheader>
+        </HeaderContainer>
+    )
+}
+
+export default Header
--- my-app/src/Components/index.jsx
+++ my-app/src/Components/index.jsx
@@ -1,6 +1,6 @@
-import { Header } from './Header'
-import { Footer } from './Footer';
-import { Navigation } from './Navigation';
-import { Projects } from './Projects';
+import Header from './Header'
+import Footer from './Footer';
+import Navigation from './Navigation';
+import Projects from './Projects';
 
 export { Header, Footer, Navigation, Projects }
